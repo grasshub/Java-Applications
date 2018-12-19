d4a46f7553e32592267241dd055a496c78634c3d
Hong Yu <umyuh1@gmail.com>
2018-12-18 22:57:06 -0500

    Commit the changes to remote repository.

--- Java SE Development/src/org/hong/javafundamental/algorithm/Matrix.java
+++ Java SE Development/src/org/hong/javafundamental/algorithm/Matrix.java
@@ -6,6 +6,7 @@ public class Matrix {
 	private int columns;
 	
 	private int[][] elements;
+	private static final String WHITE_SPACE = " ";
 	
 	public static final int ZERO = 0;
 	
@@ -32,16 +33,14 @@ public class Matrix {
 			while (startingColumnIndex < endingColumnIndex && startingRowIndex < endingRowIndex) {
 				// Print the first row from left to right from remaining rows of matrix
 				for (iterator = startingColumnIndex; iterator < endingColumnIndex; iterator++) {
-					System.out.print(elements[startingRowIndex][iterator]);
-					System.out.print(" ");
+					System.out.print(elements[startingRowIndex][iterator] + WHITE_SPACE);
 				}
 				// Increase the startingRowIndex to mark down the printing of first row
 				startingRowIndex++;
 				
 				// Print the last column from top to bottom from the remaining columns of matrix
 				for (iterator = startingRowIndex; iterator < endingRowIndex; iterator++) {
-					System.out.print(elements[iterator][endingColumnIndex - 1]);
-					System.out.print(" ");
+					System.out.print(elements[iterator][endingColumnIndex - 1] + WHITE_SPACE);
 				}
 				// Decrease the endingColumnIndex to mark down the printing of last column
 				endingColumnIndex--;
@@ -49,8 +48,7 @@ public class Matrix {
 				// Print the last row from right to left from remaining rows of matrix
 				if (startingRowIndex < endingRowIndex) {
 					for (iterator = endingColumnIndex - 1; iterator >= startingColumnIndex; iterator--) {
-						System.out.print(elements[endingRowIndex - 1][iterator]);
-						System.out.print(" ");
+						System.out.print(elements[endingRowIndex - 1][iterator] + WHITE_SPACE);
 					}
 					// Decrease the endingRowIndex to mark down the printing of last row
 					endingRowIndex--;
@@ -59,8 +57,7 @@ public class Matrix {
 				// Print the first column from bottom to top from the remaining columns of matrix
 				if(startingColumnIndex < endingColumnIndex) {
 					for (iterator = endingRowIndex - 1; iterator >= startingRowIndex; iterator--) {
-						System.out.print(elements[iterator][startingColumnIndex]);
-						System.out.print(" ");
+						System.out.print(elements[iterator][startingColumnIndex] + WHITE_SPACE);
 					}
 					// Decrease the endingColumnIndex to mark down the printing of last column
 					startingColumnIndex++;
